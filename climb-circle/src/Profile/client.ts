0c80602208a083875aa6184a819169e29e18a4d7
Annie Zhang <zhang.annie1@northeastern.edu>
2024-12-08 18:34:59 -0500

    add profile client.ts

---
+++ climb-circle/src/Profile/client.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const PROFILE_API = `${REMOTE_SERVER}/api/users`;
+
+export const isFollowing = async (userId: any) => {
+    console.log("isFollowing");
+    const response = await axios.get(`${PROFILE_API}/${userId}/isFollowing`);
+    console.log(response);
+    return response.data.isFollowing;
+};
+
+export const followUser = async (userId: any) => {
+    console.log("followUser");
+    await axios.post(`${PROFILE_API}/${userId}/follow`);
+};
+
+export const unfollowUser = async (userId: any) => {
+    console.log("unfollowUser");
+    await axios.post(`${PROFILE_API}/${userId}/unfollow`);
+};
